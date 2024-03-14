0a0ef65934735e0b184409ebe035cc0a46832349
marappoli <marappoli@outlook.com>
2024-03-14 14:56:24 -0300

    Add existing file

---
+++ Assets/scripts/Ball.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour{
+    [SerializeField]
+    private float speed = 100f;
+    private Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start(){
+        body = GetComponent<Rigidbody2D>();
+        body.velocity = Vector2.up * speed;
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+
+    float HitFactor(Vector2 ball, Vector2 player, float playerWidth){
+        //-1 -0.5 0 0.5 1
+        return(ball.x - player.x) / playerWidth;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D col){
+
+        if(col.gameObject.name == "Racket"){
+
+            //descobrir o valor de x
+            float x = HitFactor(
+                transform.position,
+                col.transform.position,
+                col.collider.bounds.size.x); 
+
+            //calcula a direção da bola
+            Vector2 dir = new Vector2(x, 1).normalized;
+
+            //velocidade da bola
+            body.velocity = dir * speed;
+
+
+
+        }
+
+    }
+}
---
+++ Assets/scripts/Racket.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+    [SerializeField]
+    private float speed = 150f;
+    private Rigidbody2D body;
+
+    // Use this for initialization
+    void Start (){
+        body = GetComponent<Rigidbody2D>();
+       
+    }
+
+    // Update is called once per frame
+    void Update (){
+        
+    }
+
+    private void FixedUpdate(){
+        float h = Input.GetAxis("Horizontal");
+        body.velocity = Vector2.right * h * speed;
+    }
+}
